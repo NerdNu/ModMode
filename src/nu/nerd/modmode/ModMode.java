c3401c35ebb780b020d3b1636d40c2675a2c6a84
Dumbo52 <dumbofiftytwo@gmail.com>
2013-02-05 20:34:46 -0500

    Remove potion effects upon entering/exiting ModMode

--- src/nu/nerd/modmode/ModMode.java
+++ src/nu/nerd/modmode/ModMode.java
@@ -5,6 +5,7 @@ import de.bananaco.bpermissions.api.util.CalculableType;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import net.minecraft.server.v1_4_6.EntityPlayer;
